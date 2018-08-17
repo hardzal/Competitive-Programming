54b0485958a9e2191c98f55a0e436972e2858a9d
hardzal <rizaldoeta98@gmail.com>
2018-08-17 22:33:14 +0700

    update

--- Toki/Kelas-Pembelajaran-Pemrograman/Pemrograman-Dasar/13-Pendalaman-Rekursi/B.cpp
+++ Toki/Kelas-Pembelajaran-Pemrograman/Pemrograman-Dasar/13-Pendalaman-Rekursi/B.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 void gunungTingkat(int n) {
 	int i, j;
-	if(n>1){
-		gunungTingkat(n-1);
+	if(n > 1){
+		gunungTingkat(n - 1);
 	}
-	for(i = 0; i<n; i++) {
+	for(i = 0; i < n; i++) {
 		cout << "*";
 	}
 	cout << endl;
-	if(n>1) {
-		gunungTingkat(n-1);
+	if(n > 1) {
+		gunungTingkat(n - 1);
 	}
 }
 
 int main() {
-	int i, j,x;
+	int i, j, x;
 	cin >> x;
 	gunungTingkat(x);
 	return 0;
